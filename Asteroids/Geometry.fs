041449dccb95c33561dd7cfd5e11cc76d5fdbb3d
Jason Ebbin <jebbin@scottlogic.co.uk>
2015-05-18 13:44:38 +0100

    Added vector rotation function and angle units of measure

--- Asteroids/Geometry.fs
+++ Asteroids/Geometry.fs
@@ -1,5 +1,20 @@
 ﻿module Geometry
 
+open System
+[<Measure>] type radian
+[<Measure>] type degree
+
+let convertDegreeToRadian (angle : float<degree>) : float<radian> = 
+    let radiansPerDegree  = (Math.PI * 1.0<radian>) / 180.0<degree>
+    radiansPerDegree * angle
+
 type Point = { X: float; Y: float }
 type Vector = { Dx: float; Dy: float }
+
+let rotate angle v =
+    let radians = float <| convertDegreeToRadian angle
+    let cos = Math.Cos(radians)
+    let sin = Math.Sin(radians)
+    { Dx = v.Dx * cos - v.Dy * sin; Dy = v.Dx * sin + v.Dy * cos }
+
 
