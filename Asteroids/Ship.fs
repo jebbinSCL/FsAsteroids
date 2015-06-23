d8a8df481625a0a2ee60f43167a50a185de35673
Jason Ebbin <jebbin@scottlogic.co.uk>
2015-06-23 13:35:40 +0100

    Add neutralPosition Value

--- Asteroids/Ship.fs
+++ Asteroids/Ship.fs
@@ -12,6 +12,7 @@ type Ship = {
     Thrust: Acceleration
 } 
 
+let neutralPosition = {X = 0.0; Y = 0.0;}
 let neutralHeading = 0.0<degree>
 let neutralRotationalVelocity = 0.0<degree>
 let neutralVelocity = {Dx = 0.0; Dy = 0.0} 
@@ -19,7 +20,7 @@ let neutralThrust = Neutral
 
 let initialShip =     
     { 
-        Position = {X = 0.0; Y = 0.0;}
+        Position = neutralPosition
         BodyWrtOrigin = 
             {
             P1 ={Color={R=0.2; G=0.9; B=1.0}; Point= {X = 0.0; Y = 0.1;}}; 
